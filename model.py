d53b40e73e5ad2333d45ca65a477787454ce7ff1
Rachel <rachelsybille@gmail.com>
2021-05-01 21:33:10 +0000

    Finish model.py outline

--- model.py
+++ model.py
@@ -50,13 +50,19 @@ class Band_users(db.Model):
 
     band_id = db.Column(db.Integer, nullable=False, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    email = db.Column(db.Text, nullable=False) 
-    display_name = db.Column(varchar(50), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.Text, nullable=False)
-    influences = db.Column(varchar(250), nullable=False)
-    location = db.Column(varchar(50), nullable=False)
-    description = db.Column(varchar(250), nullable=False)
+
+    #relationship to users
+
+    def __repr__(self):
+        pass
+
+class Musician_users(db.Model):
+    """Data model for a musician user."""
+
+    __tablename__ = "musician_users"
+
+    musician_id = db.Column(db.Integer, nullable=False, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 
     #relationship to users
 
@@ -77,13 +83,42 @@ class BandGenres(db.Model):
     def __repr__(self):
         pass
 
+class MusicianGenres(db.Model):
+    """Intermediate table connecting musician_users and musician_user_genres"""
+
+    __tablename__ = "MusicianGenres"
+
+    musician_genres = db.Column(db.Text, nullable=False, primary_key=True)
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.genre_id'), nullable=False)
+    musician_id = db.Column(db.Integer, db.ForeignKey('musician_users.musician_id'), nullable=False)
+
+    #relationships to genres and musician_users
+
+    def __repr__(self):
+        pass
+
 class Band_user_genres(db.Model):
     """Contains the selected genres of a given band user."""
 
     __tablename__ = "band_user_genres"
 
-    ##do i need foreign key
-    band_genre = db.Column(db.Text, nullable=False, primary_key=True)
+    band_user_genres = db.Column(db.Text, nullable=False, primary_key=True)
+    band_genres = db.Column(db.Text, db.ForeignKey('BandGenres.band_genres'), nullable=False)
+
+    #relationship to BandGenre
+
+    def __repr__(self):
+        pass
+
+class Musician_user_genres(db.Model):
+    """Contains the selected genres of a given musician user."""
+
+    __tablename__ = "musician_user_genres"
+
+    musician_user_genres = db.Column(db.Text, nullable=False, primary_key=True)
+    musician_genres = db.Column(db.Text, db.ForeignKey('MusicianGenres.musician_genres'), nullable=False)
+
+    #relationship to MusicianGenre
 
     def __repr__(self):
         pass
@@ -93,10 +128,25 @@ class BandSkills(db.Model):
 
     __tablename__ = "BandSkills"
 
-    ##what is the primary key going to be 
     band_skills = db.Column(db.Integer, nullable=False, primary_key=True)
     skill_id = db.Column(db.Integer, db.ForeignKey('all_skills.skill_id'), nullable=False)
     band_id = db.Column(db.Integer, db.ForeignKey('band_users.band_id'), nullable=False)
+
+    #relationships to all_skills and band_users
+
+    def __repr__(self):
+        pass
+
+class MusicianSkills(db.Model):
+    """Intermediate table connecting musician_users and musician_user_skills."""
+
+    __tablename__ = "MusicianSkills"
+
+    musician_skills = db.Column(db.Integer, nullable=False, primary_key=True)
+    skill_id = db.Column(db.Integer, db.ForeignKey('all_skills.skill_id'), nullable=False)
+    musician_id = db.Column(db.Integer, db.ForeignKey('musician_users.musician_id'), nullable=False)
+
+    #relationships to all_skills and musician_users
 
     def __repr__(self):
         pass
@@ -106,32 +156,19 @@ class Band_user_skills(db.Model):
 
     __tablename__ = "band_user_skills"
 
-    ##what is the primary key going to be 
-    ##do i need forgein key
     band_user_skills = db.Column(db.Text, nullable=False, primary_key=True)
-    band_skills = db.Column(db.Text, db.ForeignKey('BandSkills.bandskills'), nullable=False)
+    band_skills = db.Column(db.Text, db.ForeignKey('BandSkills.band_skills'), nullable=False)
 
     def __repr__(self):
         pass
 
-class Musician_users(db.Model):
-    """Data model for a musician user."""
+class Musician_user_skills(db.Model):
+    """Contains the selected skills of a given musician user."""
 
-    __tablename__ = "musician_users"
+    __tablename__ = "musician_user_skills"
 
-    musician_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    genre_id = db.Column(db.Integer, db.ForeignKey('genres.genre_id'), nullable=False)
-    email = db.Column(db.Text, nullable=False) 
-    display_name = db.Column(varchar(50), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.Text, nullable=False)
-    influences = db.Column(varchar(250), nullable=False)
-    location = db.Column(varchar(50), nullable=False)
-    description = db.Column(varchar(250), nullable=False)
-
-
-    #relationship to users and genres
+    musician_user_skills = db.Column(db.Text, nullable=False, primary_key=True)
+    musician_skills = db.Column(db.Text, db.ForeignKey('BandSkills.musician_skills'), nullable=False)
 
     def __repr__(self):
         pass
